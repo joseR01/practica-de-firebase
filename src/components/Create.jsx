02d1c3d616738b27acbb9d2bb78600edc692ba7f
Jose Rosales <josegregorio.5019@gmail.com>
2022-12-02 10:30:10 -0400

    create y edit terminado

--- src/components/Create.jsx
+++ src/components/Create.jsx
@@ -1,7 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
+import { addDoc, collection } from "firebase/firestore";
+import { db } from "./firebaseConfig/firebase";
+import { useNavigate } from "react-router-dom";
 
 const Create = () => {
-  return <div>Create</div>;
+  const [description, setDescription] = useState("");
+  const [stock, setStock] = useState("");
+  const navigate = useNavigate();
+
+  const productsCollection = collection(db, "products");
+
+  async function create(e) {
+    e.preventDefault();
+
+    await addDoc(productsCollection, {
+      description: description,
+      stock: stock,
+    });
+    navigate("/");
+  }
+
+  return (
+    <div>
+      <form onSubmit={(e) => create(e)}>
+        <div className="mb-3">
+          <label className="form-label">description</label>
+          <input
+            type="text"
+            className="form-control"
+            aria-describedby="emailHelp"
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
+          />
+        </div>
+        <div className="mb-3">
+          <label className="form-label">stock</label>
+          <input
+            type="text"
+            className="form-control"
+            aria-describedby="emailHelp"
+            value={stock}
+            onChange={(e) => setStock(e.target.value)}
+          />
+        </div>
+        <button type="submit" className="btn btn-primary">
+          Submit
+        </button>
+      </form>
+    </div>
+  );
 };
 
 export default Create;
--- src/components/Edit.jsx
+++ src/components/Edit.jsx
@@ -1,7 +1,71 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import { updateDoc, getDoc, doc } from "firebase/firestore";
+import { db } from "./firebaseConfig/firebase";
 
 const Edit = () => {
-  return <div>Edit</div>;
+  const [description, setDescription] = useState("");
+  const [stock, setStock] = useState("");
+  const navigate = useNavigate();
+  const { id } = useParams();
+  const newId = id.slice(1);
+
+  async function update(e) {
+    e.preventDefault();
+    const products = doc(db, "products", newId);
+    const data = {
+      description: description,
+      stock: stock,
+    };
+    await updateDoc(products, data);
+    navigate("/");
+  }
+
+  const getProduct = async (id) => {
+    const product = await getDoc(doc(db, "products", newId));
+
+    if (product.exists) {
+      const { description, stock } = product.data();
+      setDescription(description);
+      setStock(stock);
+    } else {
+      console.log("no existe");
+    }
+  };
+
+  useEffect(() => {
+    getProduct(id);
+  }, []);
+
+  return (
+    <div>
+      <form onSubmit={(e) => update(e)}>
+        <div className="mb-3">
+          <label className="form-label">description</label>
+          <input
+            type="text"
+            className="form-control"
+            aria-describedby="emailHelp"
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
+          />
+        </div>
+        <div className="mb-3">
+          <label className="form-label">stock</label>
+          <input
+            type="text"
+            className="form-control"
+            aria-describedby="emailHelp"
+            value={stock}
+            onChange={(e) => setStock(e.target.value)}
+          />
+        </div>
+        <button type="submit" className="btn btn-primary">
+          Submit
+        </button>
+      </form>
+    </div>
+  );
 };
 
 export default Edit;
